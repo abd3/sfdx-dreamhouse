8a4e5a1e36e59cf4330144f98ad686006b5c626a
Dave Carroll <djcarroll@gmail.com>
2016-11-09 14:28:31 -0800

    added PropertyStatus component

--- force-app/main/default/aura/GetPicklistValues/GetPicklistValuesController.js
+++ force-app/main/default/aura/GetPicklistValues/GetPicklistValuesController.js
@@ -1,6 +1,13 @@
-
 ({
-	myAction : function(component, event, helper) {
-		
+	init: function(component, event, helper) {
+		var action = component.get("c.getPicklistValues");
+		action.setParams({
+			"objectName" : component.get("v.sobject"),
+			"fieldName" : component.get("v.picklistfield")
+		});
+		action.setCallback(this, function(a) {
+            component.set("v.values", a.getReturnValue());
+    	});
+    	$A.enqueueAction(action);
 	}
 })
